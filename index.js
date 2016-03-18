7c55fafd23013601781ee3d5d6025c43dd1bcf5c
alice-em <the.alice.maldonado@gmail.com>
2016-03-17 20:59:01 -0700

    Docs for require() api's

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-var app = require('app');
-var BrowserWindow = require('browser-window');
+var app = require('app'); // http://electron.atom.io/docs/v0.37.2/api/app/
+var BrowserWindow = require('browser-window'); //  http://electron.atom.io/docs/v0.37.2/api/browser-window/
+var cbr = require('cbr'); // https://www.npmjs.com/package/cbr
+var ebook = require('ebook-cover-generator'); // https://www.npmjs.com/package/ebook-cover-generator
+var node7z = require('node-7z'); // https://www.npmjs.com/package/node-7z
+var node7zEsf = require('node-7z-esf'); // https://www.npmjs.com/package/node-7z-esf
+var rarToZip = require('rar-to-zip'); // https://www.npmjs.com/package/rar-to-zip
+var fs = require('fs'); // https://nodejs.org/api/fs.html
+var path = require('path'); // https://nodejs.org/api/path.html
 
 app.on('ready', function() {
 	var mainWindow = new BrowserWindow({
