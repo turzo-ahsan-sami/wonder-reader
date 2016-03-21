5e7596e467d5813d6cf46039e8d24fc1ef7cb464
alice-em <the.alice.maldonado@gmail.com>
2016-03-20 21:10:03 -0700

    stylings and wireframing, pt II

---
+++ app/js/jquery-app.js
@@ -0,0 +1,15 @@
+//  js/jquery-app.js
+//  Using jQuery-2.2.2
+$( document ).ready(function() {
+
+// -----<
+// Library Modifications and Stylings
+// -----<
+
+// Class Names and Font Awesome
+$(".file").before("<i class=\"fa fa-file-o\"></i> ");
+$(".dir").before("<i class=\"fa fa-folder-o\"></i> ");
+$(".dir-open").before("<i class=\"fa fa-folder-open-o\"></i> ");
+
+
+});
