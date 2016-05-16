6dfc109f05083fdd40dc80002596019cadb22763
alice-em <the.alice.maldonado@gmail.com>
2016-05-15 20:20:13 -0700

    updates using electron 1.0.2

---
+++ app/js/library.js
@@ -0,0 +1,73 @@
+var remote = require('remote');
+var fs = remote.require('fs');
+var util = require('util');
+var stream = require('stream');
+var es = require('event-stream'); // https://github.com/dominictarr/event-stream
+var dialog = remote.require('dialog');
+var $ = require('jquery');
+var cbr = require('cbr');
+
+var lineNr = 1;
+
+var filePiper(fileName, err) { // Streams files passed through the program.
+  console.log('filePiper ln1 :: ' + fs.createReadStream(fileName));
+  var stream = fs.createReadStream(fileName);
+  console.log('filePiper ln2 :: ' +  stream.pipe());
+  stream.pipe();
+  catch(err) {
+    console.log('filePiper failure.');
+  };
+});
+
+function openFile() {
+  dialog.showOpenDialog(
+    { filters: [{
+      name: 'cbr',
+      extensions: ['cbr']
+      }]
+    },
+
+    // Open File function
+    function(fileNames) {
+      console.log(fileNames); //logs file in dev tools console
+      if (fileNames === undefined) return;
+      var fileName = fileNames[0];
+      filePiper(fileName);
+
+
+
+
+
+
+
+
+    //   var s = fs.createReadStream('fileName')
+    //     .pipe(es.split())
+    //     .pipe(es.mapSync(function(line) {
+    //         // s.pause(); // Pauses readStream
+    //         // lineNr += 1; // Increments lineNr +1
+    //         // logMemoryUsage(lineNr); // Process line here and call s.resume() when ready
+    //         // s.resume(); // Resumes readStream
+    //       })
+    //       // .on('error', function() {
+    //       // 	console.log('Error while reading file');
+    //       // })
+    //       // .on('end', function() {
+    //       // 	console.log('Read file successful')
+    //       // })
+    //     );
+    //
+    //
+    //     fs.readFile(fileName, 'utf-8', function(err, data) {
+    //       if (err) throw err;
+    //       console.log('Line 103 good!')
+    //       // s(fileName);
+    //       console.log(s(fileName));
+    //       console.log('IZ GUD!')
+    //
+    //       console.log(fileName); // example: "/home/user/(comic).cbr"
+    //       console.log(data);
+    //     });
+    // }
+  );
+}
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var app = require('app'); // http://electron.atom.io/docs/v0.37.2/api/app/
-var BrowserWindow = require('browser-window'); //  http://electron.atom.io/docs/v0.37.2/api/browser-window/
+const electron = require('electron');
+const {app} = electron; // http://electron.atom.io/docs/v0.37.2/api/app/
+const {BrowserWindow} = electron; //  http://electron.atom.io/docs/v0.37.2/api/browser-window/
 // var cbr = require('cbr'); // https://www.npmjs.com/package/cbr
 // var ebook = require('ebook-cover-generator'); // https://www.npmjs.com/package/ebook-cover-generator
 // var node7z = require('node-7z'); // https://www.npmjs.com/package/node-7z
@@ -9,11 +10,28 @@ var fs = require('fs'); // https://nodejs.org/api/fs.html
 // var path = require('path'); // https://nodejs.org/api/path.html
 // var JSZip = require('jszip'); // http://stuk.github.io/jszip/
 
-app.on('ready', function() {
-	var mainWindow = new BrowserWindow({
-		width: 800,
-		height: 600
-	});
 
-	mainWindow.loadURL('file://' + __dirname + '/app/index.html'); // TODO: make into library html.
+let win // Global 'win' variable
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,  // Window Size : width (duh)
+    height: 600 // Window Size : height
+  });
+  win.loadURL('file://${__dirname}/app/index.html');
+  win.webContents.openDevTools();
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+app.on('activate', () => {
+  if (win ===null) {
+    createWindow();
+  }
 });
