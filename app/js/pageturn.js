2f1e604c56738cdf825e4411e8255b1be0c3deb7
alice-em <the.alice.maldonado@gmail.com>
2016-07-25 09:28:10 -0700

    updates

--- app/js/pageturn.js
+++ app/js/pageturn.js
@@ -5,7 +5,7 @@ var rightKey = 39;
 function pathFinder(imgID) { // finds the Array value of current loaded comic page
   var pathSource = document.getElementById(imgID).src.substr(7); // returns file:// + $PATH
   console.log(pathSource + " at line 7, pageturn.js");
-  var fileNom = pathSource.replace(/^.*[\\\/]/, ''); // Returns file name without directory path
+  var fileNom = pathSource.replace(/^.*[\\\/]/, '').replace(/%20/, ' '); // Returns file name without directory path
   console.log(fileNom + " at line 9, pageturn.js");
   var r = /[^\/]*$/;
   var pathDir = pathSource.replace(r, ''); // Returns directory path
@@ -16,10 +16,9 @@ function pathFinder(imgID) { // finds the Array value of current loaded comic pa
   return indexVar;
 };
 
-
-
 function pageRight() {
-  if (/*event.keyCode = rightKey && */(x + 2) < dirContents.length) {
+  var x = pathFinder('viewImgOne');
+  if ((x + 2) < dirContents.length) {
     x = x + 2;
     console.log('x = ' + x + ': line 4');
   } else if ((x + 2) <= dirContents.length){
@@ -30,7 +29,8 @@ function pageRight() {
   document.getElementById("viewImgTwo").src = 'cache/' + dirContents[x+1]; // Loads array[x + 1] into window
 };
 function pageLeft() {
-  if (/*event.keyCode = leftKey && */(x - 2) > 0) {
+  var x = pathFinder('viewImgOne');
+  if ((x - 2) > 0) {
     x = x - 2;
     console.log('x = ' + x + ': line 43');
   } else if ((x - 2) < 0 ) {
