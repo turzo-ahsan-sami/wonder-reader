bbe1443150bc8f61e3be5d6b51ab11856abbac03
alice-em <the.alice.maldonado@gmail.com>
2016-07-27 15:26:00 -0700

    logo.png added

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 const electron = require('electron');
 const app = electron.app; // http://electron.atom.io/docs/v0.37.2/api/app/
+// const Tray = electron.Tray; // http://electron.atom.io/docs/api/tray/
 const BrowserWindow = electron.BrowserWindow; //  http://electron.atom.io/docs/v0.37.2/api/browser-window/
 var fs = require('fs'); // https://nodejs.org/api/fs.html
-// var cbr = require('cbr'); // https://www.npmjs.com/package/cbr
-// var ebook = require('ebook-cover-generator'); // https://www.npmjs.com/package/ebook-cover-generator
-// var node7z = require('node-7z'); // https://www.npmjs.com/package/node-7z
-// var node7zEsf = require('node-7z-esf'); // https://www.npmjs.com/package/node-7z-esf
-// var rarToZip = require('rar-to-zip'); // https://www.npmjs.com/package/rar-to-zip
-// var path = require('path'); // https://nodejs.org/api/path.html
-// var JSZip = require('jszip'); // http://stuk.github.io/jszip/
-
 
 let win // Global 'win' variable
-
 function createWindow() {
   win = new BrowserWindow({
     width: 1000,  // Window Size : width (duh)
-    height: 700 // Window Size : height
+    height: 700, // Window Size : height
+    icon: './logo.png'
   });
   win.loadURL(`file://${__dirname}/app/index.html`); // Points to the html file to load in the app
   win.webContents.openDevTools(); // Loads with Dev Tools open.  Remove before release.
@@ -25,7 +18,11 @@ function createWindow() {
   });
 }
 
-app.on('ready', createWindow);
+app.on('ready', () => {
+  // const appIcon = new Tray('./logo.png')
+  // console.log(appIcon)
+  createWindow()
+});
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
