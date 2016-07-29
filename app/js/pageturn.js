36d2f42235c66091b1c11eb39ca1daaa1cfb58b8
alice-em <the.alice.maldonado@gmail.com>
2016-07-29 16:25:52 -0700

    added keypress events

--- app/js/pageturn.js
+++ app/js/pageturn.js
@@ -1,10 +1,6 @@
 var path = require('path');
 var fs = require('fs');
 
-// KeyCode Variables
-var leftKey = 37;
-var rightKey = 39;
-
 function pageRight() {
   var filePath = decodeURI(document.getElementById('viewImgOne').src.substr(7)); // removes file:// from PATH
   var fileDir = path.dirname(filePath)
@@ -16,13 +12,13 @@ function pageRight() {
     // If page number + 2 is less than the total number of pages, then it turns the page 'right'
     index = index + 2;
     console.log('x = ' + index + ': line 16');
-  } else if ((index + 2) >= dirArray.length){
+  } else if ((index + 2) >= dirArray.length) {
     // If page number + 2 is greater or equal to the total number of pages, then it stops on the last page
     index = dirArray.length;
     console.log('x = ' + index + ': line 19');
   };
-  document.getElementById("viewImgOne").src = fileDir + '/' + dirArray[index];   // Loads array[x] into window
-  document.getElementById("viewImgTwo").src = fileDir + '/' + dirArray[index + 1]; // Loads array[x + 1] into window
+  document.getElementById("viewImgOne").src = path.join(fileDir, dirArray[index]); // Loads array[x] into window
+  document.getElementById("viewImgTwo").src = path.join(fileDir, dirArray[index + 1]); // Loads array[x + 1] into window
 };
 
 function pageLeft() {
@@ -36,11 +32,23 @@ function pageLeft() {
     // If page number - 2 is greater than 0, ie the first page/cover, then it turns the page 'left'
     index = index - 2;
     console.log('x = ' + index + ': line 43');
-  } else if ((index - 2) <= 0 ) {
+  } else if ((index - 2) <= 0) {
     // If page number - 2 is less or equal to 0, ie the first page/cover, then it stops on the first page/cover
     index = 0;
     console.log('x = ' + index + ': line 46');
   };
-  document.getElementById("viewImgOne").src = fileDir + '/' + dirArray[index];    // Loads array[x] into window
-  document.getElementById("viewImgTwo").src = fileDir + '/' + dirArray[index + 1];  // Loads array[x + 1] into window
+  document.getElementById("viewImgOne").src = path.join(fileDir, dirArray[index]); // Loads array[x] into window
+  document.getElementById("viewImgTwo").src = path.join(fileDir, dirArray[index + 1]); // Loads array[x + 1] into window
 };
+
+// KeyCode Variables
+var leftKey = 37;
+var rightKey = 39;
+
+$(document).keydown(function(event) {
+  if (event.which == leftKey) {
+    pageLeft();
+  } else if (event.which == rightKey) {
+    pageRight();
+  };
+});
