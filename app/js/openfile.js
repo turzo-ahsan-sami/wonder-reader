cd4f302f6216babf1cc3df1e6c64e4823b948f8f
alice-em <the.alice.maldonado@gmail.com>
2016-08-02 13:05:09 -0700

    .cbz support added

--- app/js/openfile.js
+++ app/js/openfile.js
@@ -12,9 +12,16 @@ var page = require('./js/pageturn.js'); // Page turning functionality
 function filePiper(fileName, err) { // Streams files passed through the program.
 
   // Folder Creation
-  var fileComic = path.posix.basename(fileName, '.cbr'); // Function removes path dir, spaces, and '#'. Good to note!
-  if (process.platform == 'win32') {
-    fileComic = path.win32.basename(fileName, '.cbr');
+  if (path.extname(fileName) == ".cbr") {
+    var fileComic = path.posix.basename(fileName, '.cbr'); // Function removes path dir, spaces, and '#'. Good to note!
+    if (process.platform == 'win32') {
+      fileComic = path.win32.basename(fileName, '.cbr');
+    }
+  } else if (path.extname(fileName) == ".cbz") {
+    var fileComic = path.posix.basename(fileName, '.cbz'); // Function removes path dir, spaces, and '#'. Good to note!
+    if (process.platform == 'win32') {
+      fileComic = path.win32.basename(fileName, '.cbz');
+    }
   }
   console.log(fileComic);
   var tempFolder = path.join("app/cache/", fileComic); // tempFolder Variable for loaded comic
@@ -22,23 +29,36 @@ function filePiper(fileName, err) { // Streams files passed through the program.
   mkdirp.sync(tempFolder);
   console.log('CREATE: ' + tempFolder + ' created, line 18');
 
-  var rar = new unrar(fileName);
-  rar.extract(tempFolder, null, function (err) {
-    console.log('Line 29 Success!');
-    var dirContents = fs.readdirSync(tempFolder);
-    $('#loader').addClass('hidden').removeClass('loader');
-    document.getElementById("viewImgOne").src = path.join('cache/', fileComic, dirContents[0]); // Loads array[0] into window
-    document.getElementById("viewImgTwo").src = path.join('cache/', fileComic, dirContents[1]); // Loads array[1] into window
-    libWatch.load(fileName); // libwatch.js
-  });
+  if (path.extname(fileName) == ".cbr") {
+    var rar = new unrar(fileName);
+    rar.extract(tempFolder, null, function (err) {
+      console.log('Line 29 Success!');
+      var dirContents = fs.readdirSync(tempFolder);
+      $('#loader').addClass('hidden').removeClass('loader');
+      document.getElementById("viewImgOne").src = path.join('cache/', fileComic, dirContents[0]); // Loads array[0] into window
+      document.getElementById("viewImgTwo").src = path.join('cache/', fileComic, dirContents[1]); // Loads array[1] into window
+      libWatch.load(fileName); // libwatch.js
+    });
+  } else if (path.extname(fileName) == ".cbz") {
+    extract(fileName, {dir: 'app/cache'}, function(err) {
+      console.log('Line 29 Success!');
+      var dirContents = fs.readdirSync(tempFolder);
+      $('#loader').addClass('hidden').removeClass('loader');
+      document.getElementById("viewImgOne").src = path.join('cache/', fileComic, dirContents[0]); // Loads array[0] into window
+      document.getElementById("viewImgTwo").src = path.join('cache/', fileComic, dirContents[1]); // Loads array[1] into window
+      libWatch.load(fileName); // libwatch.js
+    });
+  } else {
+    alert("I don't think that is a comic you picked.");
+  };
   $('#loader').addClass('loader').removeClass('hidden');
 };
 
 function openFile() {
   dialog.showOpenDialog( // Limits openFile to .cbr files
     { filters: [{
-      name: 'cbr',
-      extensions: ['cbr']
+      name: 'Comic Files (.cbr, .cbz)',
+      extensions: ['cbr', 'cbz']
       }]
     },
 
