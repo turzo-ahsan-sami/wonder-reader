029435103629f54b7c596eff770fc80b1f128ed5
alice-em <the.alice.maldonado@gmail.com>
2016-08-04 11:36:51 -0700

    slider/text input connections

--- app/js/zoom.js
+++ app/js/zoom.js
@@ -1,34 +1,36 @@
-var viewWindow = document.getElementById('view-window');
-var zoomLimit = document.getElementById('zoomlimit');
-var viewHeight = parseInt(document.getElementById('view-window').style.height);
-var viewWidth = parseInt(document.getElementById('view-window').style.width);
-  // var vertMargin = parseInt(document.getElementById('view-window').style.marginTop)
-function limiter() {
+var viewer = document.getElementById('viewer'); // View Window
+var zoomSlider = document.getElementById('zoomSlider'); // Zoom Slider
+var zoomText = document.getElementById('zoomText'); // Zoom Text Input
 
-  var zoomValue = zoomLimit.value;
+var viewHeight = parseInt(viewer.style.height);
+var viewWidth = parseInt(viewer.style.width);
+  // var vertMargin = parseInt(document.getElementById('view-window').style.marginTop)
+function slideToText() {
+
+  var zoomValue = zoomSlider.value;
   // Limits zoom paramaters to 25% to 200%
   if (zoomValue >= 200) {
-    zoomLimit.value = 200;
+    zoomSlider.value = 200;
   } else if (zoomValue < 25) {
-    zoomLimit.value = 25;
+    zoomSlider.value = 25;
   } else {
     // do nothiing
   };
 
   // Inputs numbers into application
-  var newHeight = zoomLimit.value * 0.01 * viewHeight;
-  var newWidth = zoomLimit.value * 0.01 * viewWidth;
-  viewWindow.style.height = newHeight;
-  viewWindow.style.marginTop = newHeight * -0.5 + 20;
-  viewWindow.style.width = newWidth;
-  viewWindow.style.marginLeft = newWidth * -0.5;
+  var newHeight = zoomSlider.value * 0.01 * viewHeight;
+  var newWidth = zoomSlider.value * 0.01 * viewWidth;
+  viewer.style.height = newHeight;
+  viewer.style.marginTop = newHeight * -0.5 + 20;
+  viewer.style.width = newWidth;
+  viewer.style.marginLeft = newWidth * -0.5;
 };
 
 /* Prototype value increaser */
-var c = 0
-
-function zoomAdder () {
-  var zoomValue = parseInt(document.getElementById('zoomlimit').value)
-  // console.log("++c + zoomValue is "++c + zoomValue);
-  document.getElementById("zoomlimit").value = zoomValue + ++c;
-};
+// var c = 0
+//
+// function zoomAdder () {
+//   var zoomValue = parseInt(document.getElementById('zoomlimit').value)
+//   // console.log("++c + zoomValue is "++c + zoomValue);
+//   document.getElementById("zoomlimit").value = zoomValue + ++c;
+// };
