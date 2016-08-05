a3cde4d52f7e515d960bf405840086d7f9c66460
alice-em <the.alice.maldonado@gmail.com>
2016-08-05 16:07:04 -0700

    center.fold() comments!

--- app/js/centerfold.js
+++ app/js/centerfold.js
@@ -1,35 +1,28 @@
 var sizeOf = require('image-size'),
-    path = require('path'),
-    fs = require('fs');
+  path = require('path'),
+  fs = require('fs');
 
 exports.fold = (id) => {
   var filePath = decodeURI(document.getElementById(id).src.substr(7)), // removes file:// from PATH
-      fileDir = path.dirname(filePath), // Directory
-      dirContents = fs.readdirSync(fileDir), // Directory
-      fileName = path.basename(filePath),
-      spread = [];
+    fileDir = path.dirname(filePath), // Directory
+    dirContents = fs.readdirSync(fileDir), // Directory
+    fileName = path.basename(filePath),
+    spread = [];
 
-      console.log(fileDir);
-      console.log(dirContents.length);
+  for (var i = 0; i < dirContents.length; i++) {
+    (function(i) {
+      var dimensions = sizeOf(path.join(fileDir, dirContents[i]));
+      var width = dimensions.width;
+      var height = dimensions.height;
 
-      console.log(sizeOf(path.join(fileDir, dirContents[0])).width);
-      console.log(sizeOf(path.join(fileDir, dirContents[0])).height);
-
-      for(var i = 0; i < dirContents.length; i++) {
-        (function(i) {
-          var dimensions = sizeOf(path.join(fileDir, dirContents[i]));
-          var width = dimensions.width;
-          var height = dimensions.height;
-
-          if ( width >= height ) {
-            spread.push(i);
-            console.log(i);
-          } else {
-            // Do Nothing!
-          };
-        })(i);
+      if (width >= height) {
+        spread.push(i);
+      } else {
+        // Do Nothing!
       };
+    })(i);
+  };
 
-      console.log(spread);
-      return spread;
+  console.log("These are the indexes of centerfolds" + spread);
+  return spread;
 }
