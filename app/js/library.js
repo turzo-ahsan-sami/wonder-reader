7c2d4fd34c8fcde6a00c8cfee15a44cca7e64c0a
alice-em <the.alice.maldonado@gmail.com>
2016-09-16 00:00:24 -0700

    library functionality and stylings

--- app/js/library.js
+++ app/js/library.js
@@ -14,21 +14,36 @@ function libBuilder(directory, array, listID) {
   for (var i=0; i < array.length; i++) {
     if (fs.statSync(path.join(directory, array[i].name)).isFile()) {
       // console.log(colors.cyan(array[i].name));
-      $('#' + listID).append('<li class="file"><i class="fa fa-file" aria-hidden="true"></i><a href="#" onclick="file.loader(\'' + path.join(directory, encodeURIComponent(array[i].name)) + '\')">' + array[i].name + '</a></li>')
+
+      newDirectory = dirEncode(directory);
+
+      $('#' + listID).append('<li class="file"><a href="#" onclick="file.loader(\'' + path.join(newDirectory, encodeURIComponent(array[i].name)) + '\')"><i class="fa fa-file" aria-hidden="true"></i>' + array[i].name + '</a></li>')
     } else if ( fs.statSync(path.join(directory, array[i].name)).isDirectory()) {
       // console.log(colors.red(array[i].name));
-      var newListID = array[i].name.replace(/\s/g, "").replace(/#/g, "");
-      $('#' + listID).append('<ul id=' + newListID + '><li class="folder"><i class="fa fa-folder" aria-hidden="true"></i><a href="#" onclick="#">' + array[i].name + '</a></li></ul>')
+
+      var newListID = array[i].name.replace(/\s|#|\(|\)|\'/g, "");
+      $('#' + listID).append('<ul id=' + newListID + '><a href="#" onclick="#"><li class="folder"><i class="fa fa-folder" aria-hidden="true"></i>' + array[i].name + '</a></li></ul>')
       libBuilder(path.join(directory, array[i].name), array[i].children, newListID)
     }
   };
 };
 
+function dirEncode(oldPath) {
+  var newPath = '';
+  var tempPath = oldPath.split(path.sep);
+
+  for (j=0; j < tempPath.length; j++) {
+    newPath = path.join(newPath, encodeURIComponent(tempPath[j]));
+  };
+
+  return newPath;
+};
+
 exports.builder = () => {
   var config = jsonfile.readFileSync(path.join(os.tmpdir(), 'wonderReader', 'json', 'config.json'));
   var directory = config.library
-  var jSon = dirTree(config.library, ['.cbz', '.cbr']);
-  var listID = 'ulLib'
+  var jSon = dirTree(directory, ['.cbz', '.cbr']);
+  var listID = 'ulLib';
 
   libBuilder(directory, jSon.children, listID)
 }
