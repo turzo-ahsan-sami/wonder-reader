74ef7e5af1c5fd983496c839e887940eef807e02
alice-em <the.alice.maldonado@gmail.com>
2016-10-24 15:31:48 -0700

    0.3.4: bookmark.js and json corrections

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Wonder-Reader
 
-Version 0.3.3
+Version 0.3.4
 
 ## Downloads
 
--- app/js/bookmark.js
+++ app/js/bookmark.js
@@ -5,12 +5,8 @@ const os = require('os');
 const page = require('./page.js');
 const path = require('path');
 
-let template = {
-  "name": "",
-  "currentIndex": "0",
-  "fullIndex": "0"
-};
 
+let template = {}; // {"name": "", "currentIndex": "0", "fullIndex": "0"};
 let baseName, index, json, obj;
 var bookmark = path.join(os.tmpdir(), 'wonderReader', 'json', 'bookmark.json');
 var regex = /\s|%|#|\(|\)|\.|-|_/gi;
@@ -18,11 +14,11 @@ var regex = /\s|%|#|\(|\)|\.|-|_/gi;
 exports.onLoad = (filePath, directoryContents) => { // returns a new index for <img> tags
   baseName = path.basename(filePath);
 
+  template = {};
   template.name = baseName;
   template.currentIndex = 0;
   template.fullIndex = directoryContents.length - 1;
-
-  template = Object.keys(template).map(function(k) { return template[k] }); // JSON => Array
+  console.log(template)
 
   if ( isThere(bookmark) ) {
     obj = jsonfile.readFileSync(bookmark);
@@ -52,12 +48,12 @@ exports.onLoad = (filePath, directoryContents) => { // returns a new index for <
 exports.onChange = (index) => {
   jsonfile.readFile(bookmark, function(err, obj) {
     if (err) {return err};
-    obj[baseName][1] = index;
+    obj[baseName].currentIndex = index;
     jsonfile.writeFile(bookmark, obj, {spaces: 2}, function(err) {
       if (err) { return err };
     });
-    let percent = obj[baseName][1]/obj[baseName][2]*100;
-    let spanClass = obj[baseName][0].replace(regex, '');
+    let percent = (obj[baseName].currentIndex/obj[baseName].fullIndex)*100;
+    let spanClass = obj[baseName].name.replace(regex, '');
 
     let elem = document.getElementsByClassName(spanClass);
     for(let i = 0; i < elem.length; i++) {
@@ -71,7 +67,8 @@ exports.percent = (fileName) => {
   if (isThere (bookmark) ) {
     obj = jsonfile.readFileSync(bookmark);
     if ( obj[fileName] ) {
-      let percent = obj[fileName][1]/obj[fileName][2]*100;
+      console.log(`${obj[fileName].currentIndex} of ${obj[fileName].currentIndex}`)
+      let percent = (obj[fileName].currentIndex/obj[fileName].currentIndex)*100;
       return `<span class="bookmark-percent ${spanClass}">${percent.toFixed(0)}%</span>`;
     } else { return `<span class="bookmark-percent ${spanClass}">0%</span>`};
   } else {
