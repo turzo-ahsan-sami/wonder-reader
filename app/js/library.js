78c1c043568e2d77e64ffac810bd2c1126007766
Alice Maldonado <the.alice.maldonado@gmail.com>
2017-02-28 10:08:24 -0800

    getting win32 testing ready

--- app/js/library.js
+++ app/js/library.js
@@ -32,6 +32,7 @@ libBuilder = (directory, array, listID) => {
     // Inserts file.loader() for files
     if (fs.statSync(filePath).isFile()) {
       let fileTarget = dirFunction.encode(filePath);
+      console.log(fileTarget);
       $(`#${listID}`).append(
         `<li class="file"><a href="#" onclick="file.loader('${fileTarget}')"><i class="fa fa-file" aria-hidden="true"></i>${file} ${bookmark.percent(file)}</a></li>`
       );
