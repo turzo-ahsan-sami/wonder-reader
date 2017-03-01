b1b0252d4194b5be92b3f821ac5fbf4523c59d6a
Alice Maldonado <the.alice.maldonado@gmail.com>
2017-03-01 15:36:13 -0800

    debugging for win32

--- app/js/library.js
+++ app/js/library.js
@@ -32,10 +32,11 @@ libBuilder = (directory, array, listID) => {
     // Inserts file.loader() for files
     if (fs.statSync(filePath).isFile()) {
       let fileTarget = dirFunction.encode(filePath);
+      console.log(fileTarget);
       if (process.platform == 'win32') { // Converts win32 paths to HTML compatible paths
         fileTarget = fileTarget.replace(/\\/g, '/');
+        console.log(fileTarget);
       }
-      // console.log(fileTarget);
       $(`#${listID}`).append(
         `<li class="file"><a href="#" onclick="file.loader('${fileTarget}')"><i class="fa fa-file" aria-hidden="true"></i>${file} ${bookmark.percent(file)}</a></li>`
       );
