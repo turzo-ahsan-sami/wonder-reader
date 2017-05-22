2e27cc07d7d2f87248104e6cc51b65528f66bfef
Alice Maldonado <the.alice.maldonado@gmail.com>
2017-05-22 11:49:05 -0700

    config.display now saves and returns onStart()

--- app/js/config.js
+++ app/js/config.js
@@ -7,7 +7,7 @@ const os = require('os');
 const path = require('path');
 
 const comics = path.join(os.tmpdir(), 'wonderReader', 'json', 'comics.json');
-const config = path.join(os.tmpdir(), 'wonderReader', 'json', 'config.json');
+const configFile = path.join(os.tmpdir(), 'wonderReader', 'json', 'config.json');
 const column = document.getElementById('column');
 const columnIcon = document.getElementById('columnIcon');
 
@@ -28,13 +28,13 @@ databaseBuild = (filePath) => {
 };
 
 configSave = (type, val) => {
-  jsonfile.readFile(config, function(err, obj) {
+  jsonfile.readFile(configFile, function(err, obj) {
     if (err) {
       console.error(err);
       return;
     }
     obj[type] = val;
-    jsonfile.writeFile(config, obj, function(err) {
+    jsonfile.writeFile(configFile, obj, function(err) {
       if (err) console.error(err);
     });
   });
@@ -43,9 +43,9 @@ configSave = (type, val) => {
 onStart = () => {
   let libStatus= document.getElementById('libStatus');
   let obj;
-  switch (isThere(config)) {
+  switch (isThere(configFile)) {
   case true:
-    jsonfile.readFile(config, function(err, obj) {
+    jsonfile.readFile(configFile, function(err, obj) {
       if (err) console.error(err);
       column.dataset.val = obj.page || 2;
       switch (Number(column.dataset.val)) {
@@ -70,9 +70,9 @@ onStart = () => {
   default:
     obj = template;
     libStatus.innerHTML = libIsEmpty;
-    mkdirp(path.dirname(config), function(err) {
+    mkdirp(path.dirname(configFile), function(err) {
       if (err) console.error(err);
-      jsonfile.writeFile(config, obj, function(err) {
+      jsonfile.writeFile(configFile, obj, function(err) {
         if (err) console.error(err);
       });
     });
@@ -81,8 +81,8 @@ onStart = () => {
 
 defaults = (prop) => {
   let obj;
-  if (isThere(config)) {
-    obj = jsonfile.readFileSync(config);
+  if (isThere(configFile)) {
+    obj = jsonfile.readFileSync(configFile);
     if (obj[prop]) {
       return obj[prop];
     } else {
@@ -101,6 +101,14 @@ exports.displaySave = (val) => {
   configSave('display', val);
 };
 
+exports.displayReturn = () => {
+  return defaults('display') || {
+    'brightness': 'brightness(1.0)',
+    'contrast': 'contrast(1.0)',
+    'grayscale': 'grayscale(0.0)'
+  };
+};
+
 exports.library = () => {
   return defaults('library') || '';
 };
@@ -111,6 +119,7 @@ exports.libSave = (filePath) => {
 
 exports.onStart = () => {
   onStart();
+  // start up options are passed through `options.js`
 };
 
 exports.pageReturn = () => {
--- app/js/options.js
+++ app/js/options.js
@@ -1,6 +1,8 @@
 const $ = require('jquery');
+const config = require('./config.js');
+const debounce = require('debounce');
 
-let toggle;
+let onInput, onStart, toggle;
 
 const inner = document.getElementById('innerWindow');
 const optWindow = document.getElementById('optWindow');
@@ -13,11 +15,12 @@ toggle = () => {
 };
 
 exports.onStart = () => {
+  let previousConfig = config.displayReturn();
   let display = {
-    'brightness': 'brightness(1.0)',
-    'contrast': 'contrast(1.0)',
-    'grayscale': 'grayscale(0.0)',
-    style: function() {
+    'brightness': previousConfig.brightness,
+    'contrast': previousConfig.contrast,
+    'grayscale': previousConfig.grayscale,
+    'style': function() {
       return `${this.brightness} ${this.contrast} ${this.grayscale}`;
     }
   };
@@ -26,17 +29,13 @@ exports.onStart = () => {
   // Option :: Updates Styles and Text
   let options = optWindow.getElementsByTagName('input');
   for (let i = 0; i < options.length; i++) {
+    onStart(options[i], display);
     options[i].addEventListener('input', function() {
-      let val = options[i].value;
-      let style = options[i].dataset.style;
-      display[style] = `${style}(${val})`;
-      inner.style.webkitFilter = display.style();
-      let Style = style.capitalize();
-      let text = document.getElementById(`opt${Style}Text`);
-      text.value = val;
+      onInput(options[i], display);
+      debounce(config.displaySave(display), 250);
     });
   }
-
+  console.log(inner.style.webkitFilter);
   // Options :: Reset Buttons
   let buttons = optWindow.getElementsByTagName('button');
   for(let i = 0; i < buttons.length; i++) {
@@ -50,8 +49,39 @@ exports.onStart = () => {
       inner.style.webkitFilter = display.style();
       range.value = range.dataset.default;
       text.value = range.dataset.default;
+      debounce(config.displaySave(display), 250);
     });
   }
+};
+
+// ----- ELEM EXAMPLE, usually targetting `input[range]`
+//
+// <label for="optContrast">Contrast</label>
+// <input type="range" min="0.1" max="5" value="1" step="0.1" id="optContrastRange" for="optContrastText" name="optContrastRange" data-default="1" data-style="contrast" />
+// <output type="text" for="optContrastRange" id="optContrastText">1</output>
+// <button id="optContrastButton" data-style="contrast">
+//   <i class="fa fa-undo" aria-hidden="true"></i>
+// </button>
+
+// This uses option[i] and extracts and updates inputs
+onInput = (elem, display) => {
+  let val = elem.value;
+  let style = elem.dataset.style;
+  display[style] = `${style}(${val})`;
+  inner.style.webkitFilter = display.style();
+  let Style = style.capitalize();
+  let text = document.getElementById(`opt${Style}Text`);
+  text.value = val;
+};
+
+// This uses options[i] to change input values onStart to that of the saved config.json[display]
+onStart = (elem, display) => {
+  let style = elem.dataset.style;
+  let regex = /[a-z]+\(|\)/gi;
+  let value = display[style].replace(regex, '');
+  let text = document.getElementById(`opt${style.capitalize()}Text`);
+  elem.value = value;
+  text.value = value;
 };
 
 exports.toggle = () => {
