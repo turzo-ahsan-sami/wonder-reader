fb5632354aa2a4a31fde48559e3b78420086b81d
alice <the.alice.maldonado@gmail.com>
2019-12-28 16:01:21 -0600

    refactor

--- app/modules/polaritySort.js
+++ app/modules/polaritySort.js
@@ -1,11 +1,12 @@
 const basenameSort = (a, b, prop) => {
   const targetProp = typeof prop !== 'undefined';
-  const selectProperty = val =>
-    targetProp ? val[prop].toLowerCase() : val.toLowerCase();
+  const selectProperty = val => targetProp ? val[prop].toLowerCase() : val.toLowerCase();
   const nameA = selectProperty(a);
   const nameB = selectProperty(b);
   const polarity = nameA < nameB ? -1 : 1;
   return nameA === nameB ? 0 : polarity;
 };
+
+const propertySort = prop => (a, b) => basenameSort(a[prop], b[prop]);
 
 export default basenameSort;
