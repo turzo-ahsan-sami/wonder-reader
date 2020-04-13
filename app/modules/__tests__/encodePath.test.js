337ac719e849dae7334f6ab8c181cf0d288ce66b
alice maldonado <the.alice.maldonado@gmail.com>
2020-04-12 22:17:58 -0500

    refactors

--- app/modules/__tests__/encodePath.test.js
+++ app/modules/__tests__/encodePath.test.js
@@ -1,10 +1,35 @@
-import { encodeUnix } from '../encodePath';
+// import encodePath, { encodeUnix, encodeWin } from '../encodePath';
+import { encodeUnix, encodeWin } from '../encodePath';
 
 describe('encodePath', () => {
   it('encodeUnix', () => {
-    const filepath = '/home/sample user/Documents/git/wonder-reader';
-    expect(encodeUnix(filepath)).toBe(
-      '/home/sample%20user/Documents/git/wonder-reader',
-    );
+    const filepath = '/home/sample user/wonder-reader';
+    expect(encodeUnix(filepath)).toBe('/home/sample%20user/wonder-reader');
   });
+  it('encodeWin', () => {
+    const filepath = 'c:/sample user/file/abc.cbr';
+    expect(encodeWin(filepath)).toBe('c:/sample%20user/file/abc.cbr');
+  });
+
+  // describe('OS Operations', () => {
+  //   it('should format the string for Unix', () => {
+  //     jest.mock('os', () => ({
+  //       _esModule: true,
+  //       platform: () => 'darwin',
+  //     }));
+  //     const filepath = '/home/sample user/wonder-reader';
+  //     expect(encodePath(filepath)).toBe('/home/sample%20user/wonder-reader');
+  //     jest.resetAllMocks();
+  //   })
+  //
+  //   it('should format the string for Windows', () => {
+  //     jest.mock('os', () => ({
+  //       _esModule: true,
+  //       platform: () => 'win32',
+  //     }));
+  //     const filepath = 'c:/sample user/file/abc.cbr';
+  //     expect(encodePath(filepath)).toBe('c:/sample%20user/file/abc.cbr');
+  //     jest.resetAllMocks();
+  //   })
+  // })
 });
